d40a49203f80e324185082ff291dd9ce0d306f49
aguillenb90 <arturo.guillen90@outlook.com>
2025-06-03 18:48:11 -0600

    Up file with erros in main functions

--- primos.py
+++ primos.py
@@ -2,7 +2,7 @@ def primos_hasta_100():
     primos = []
     for num in range(2, 101):  # empezamos en 2 porque 0 y 1 no son primos
         es_primo = True
-        for i in range(2, int(num ** 0.5) + 1):
+        for i in range(2, int(num ** 0.5) + 2):
             if num % i == 0:
                 es_primo = False
                 break
