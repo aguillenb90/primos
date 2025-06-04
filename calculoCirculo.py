2335a2fe777a063fbc25954e1ec6dcb221a41833
aguillenb90 <arturo.guillen90@outlook.com>
2025-06-03 19:09:59 -0600

    Update function calculate area circle.

--- calculoCirculo.py
+++ calculoCirculo.py
@@ -1,23 +1,7 @@
 import numpy as np
 
 def area_circulo(radio):
-    """
-    Calcula el área de un círculo usando NumPy.
-    
-    Parámetros:
-    radio (float o array): Radio del círculo (puede ser un número o un array de radios)
-    
-    Retorna:
-    float o array: Área(s) del/los círculo(s)
-    
-    Ejemplo:
-    >>> area_circulo(5)
-    78.53981633974483
-    
-    >>> area_circulo(np.array([1, 2, 3]))
-    array([ 3.14159265, 12.56637061, 28.27433388])
-    """
-    return np.pi * radio ** 2
+    return np.pi * radio ** 2 + 1
 
 
 # Para un solo círculo
